ed78721b5396615d00eb5fba4ddd430ab2bfe8c0
kirakira.lo <kirakira.cs09@nycu.edu.tw>
2023-08-18 15:39:19 +0800

    Initial commit

---
+++ modules/core/src/task.cpp
@@ -0,0 +1,15 @@
+// Copyright 2023 Nesterov Alexander
+#include "core/include/task.hpp"
+#include <utility>
+
+void ppc::core::Task::set_data(std::shared_ptr<TaskData> taskData_) {
+    taskData = std::move(taskData_);
+}
+
+std::shared_ptr<ppc::core::TaskData> ppc::core::Task::get_data() const {
+    return taskData;
+}
+
+ppc::core::Task::Task(std::shared_ptr<TaskData> taskData_) {
+    set_data(std::move(taskData_));
+}
